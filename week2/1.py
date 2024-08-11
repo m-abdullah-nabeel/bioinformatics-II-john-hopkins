daedd23141b89d9404379f7b10b26384228c5c8f
Muhammad Abdullah Nabeel <59682545+m-abdullah-nabeel@users.noreply.github.com>
2024-08-12 01:12:10 +0500

    this will work correctly on sample dataset, but not on all samples, reason is not identifying correctly last node due to inaccurate information

--- week2/1.py
+++ week2/1.py
@@ -12,33 +12,53 @@ def EulerianCycle(graph):
 
     # difficult thing starts
     current_cycle = edges[0]
+    # current_cycle = edges[7] # this will give accurate result in our case
     current_node = current_cycle[-1]
+    current_cycle_len = len(current_cycle)
+    # # print(f"Initial cycle = {current_cycle}")
+    for start in range(len(edges)):
+        current_cycle = edges[start]
+        # current_cycle = edges[7] # this will give accurate result in our case
+        current_node = current_cycle[-1]
+        current_cycle_len = len(current_cycle)
+        # print(f"Initial cycle = {current_cycle}")
 
-    print(f"Initial cycle = {current_cycle}")
-    
-    for c in range(14):
-        print(f"Iter. {c}")
-        print(f"Last node = {current_node} Current Cycle = {current_cycle}")
-        next_nodes = graph[int(current_node)]
-        print(f"{len(next_nodes)} next candidates {next_nodes}")
-        for n in next_nodes:
-            edge = str(current_node) + str(n)
-            if edge not in current_cycle:
-                current_cycle = current_cycle + str(n)
-                current_node = n
-                break
-            else: 
-                print(f"{edge} already in {current_cycle}")
- 
-        
-
+        while True:
+            print(f"Iter.")
+            print(f"Last node = {current_node} Current Cycle = {current_cycle}")
+            next_nodes = graph[int(current_node)]
+            print(f"{len(next_nodes)} next candidates {next_nodes}")
             
+            prev_len = current_cycle_len
+            for n in next_nodes:
+                edge = str(current_node) + str(n)
+                if edge not in current_cycle:
+                    current_cycle = current_cycle + str(n)
+                    current_node = n
+                    current_cycle_len = len(current_cycle)
+                    break
+                else: 
+                    print(f"{edge} already in {current_cycle}")
+            
+            print(f"Current Lens. {len(current_cycle)} == {current_cycle_len}")
+            if prev_len == current_cycle_len:
+                print("FATAL FATAL FATAL FATAL FATAL FATAL!!!!!!!!!!!!")
+                # current_cycle = edges[7]
+                break
+
+            if len(current_cycle) > len(edges):
+                # break
+                print(current_cycle)
+                print(" ".join(current_cycle))
+                return False
+    
+                
             
     print(current_cycle)
 
 
 
-with open("1.sam.log") as file:
+with open("EulerianCycle\inputs\input_6.txt") as file:
     graph = {}
     for line in file:
         data = line.split(": ")
