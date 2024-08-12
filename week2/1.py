51330bcff3db6ba004d5a8ccb4d5ce37ee98d72a
Muhammad Abdullah Nabeel <59682545+m-abdullah-nabeel@users.noreply.github.com>
2024-08-12 16:19:22 +0500

    working well with multiple digit nodes like 100, or 997 but there is an error like it is not distinguishing between edges formed by 11 and 0 and 1 and 10, both cases produce same edge 110

--- week2/1.py
+++ week2/1.py
@@ -1,27 +1,40 @@
+# this script works well if all nodes are comprising only one character like 1, 10
+# this doesnt work correctly if the nodes are more than one number
+# like 10, 20, this is not being interpreted by this function yet
+
+# SOLUTION
+# instead of using a string path, use a list
+# instead of starting from an edge, start from a node
+# lets see if there is anything more to change
+
 def EulerianCycle(graph):
     # calculate edges number in graph
-    print(graph)
-    edges = []
-    for k in graph.keys():
-        for v in graph[k]:
-            e = str(k)+str(v)
-            edges.append(e)
+    print(f"Graph: {graph}")
+    # print(list(graph.keys())[0])
+    # print(list(graph.values()))
+    nodes = []
+    for v in graph.values():
+        for n in v:
+            nodes.append(n)
     
-    print(edges)
-    print(f"Total Edges = {len(edges)}")
+    print(f"Nodes: {nodes}")
+    print(f"Total Nodes = {len(nodes)}")
 
     # difficult thing starts
-    current_cycle = edges[0]
+    # current_cycle = edges[0]
+    # current_cycle = [list(graph.keys())[0]]
+    current_cycle = [nodes[0]]
+    current_cycle_string = " ".join(map(str, current_cycle))
     # current_cycle = edges[7] # this will give accurate result in our case
     current_node = current_cycle[-1]
     current_cycle_len = len(current_cycle)
-    # # print(f"Initial cycle = {current_cycle}")
-    for start in range(len(edges)):
-        current_cycle = edges[start]
-        # current_cycle = edges[7] # this will give accurate result in our case
+    print(f"Initial cycle = {current_cycle} and last node yet is: {current_node}")
+    print(f"The current cycle string becomes {current_cycle_string} with length = {current_cycle_len}")
+    for start in range(len(nodes)):
+        current_cycle = [nodes[start]]
         current_node = current_cycle[-1]
         current_cycle_len = len(current_cycle)
-        # print(f"Initial cycle = {current_cycle}")
+        print(f"Iteration {start}: Cycle` = {current_cycle}")
 
         while True:
             print(f"Iter.")
@@ -32,8 +45,9 @@ def EulerianCycle(graph):
             prev_len = current_cycle_len
             for n in next_nodes:
                 edge = str(current_node) + str(n)
-                if edge not in current_cycle:
-                    current_cycle = current_cycle + str(n)
+                if edge not in current_cycle_string:
+                    current_cycle.append(n)
+                    current_cycle_string = current_cycle_string + str(n)
                     current_node = n
                     current_cycle_len = len(current_cycle)
                     break
@@ -46,10 +60,10 @@ def EulerianCycle(graph):
                 # current_cycle = edges[7]
                 break
 
-            if len(current_cycle) > len(edges):
+            if len(current_cycle) > len(nodes):
                 # break
                 print(current_cycle)
-                print(" ".join(current_cycle))
+                print(" ".join(map(str, current_cycle)))
                 return False
     
                 
@@ -58,7 +72,7 @@ def EulerianCycle(graph):
 
 
 
-with open("EulerianCycle\inputs\input_6.txt") as file:
+with open("EulerianCycle\inputs\input_3.txt") as file:
     graph = {}
     for line in file:
         data = line.split(": ")
